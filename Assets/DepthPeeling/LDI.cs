2d62e5d07804da63ba428f24f2ad2332f09a0636
Furkan <furkan.unsal@tedu.edu.tr>
2024-05-03 07:38:24 +0300

    depth peeling

---
+++ Assets/DepthPeeling/LDI.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class LDI : MonoBehaviour
+{
+    public Vector2Int ldi_resolution;
+    public List<RenderTexture> ldi_hierarchy = new List<RenderTexture>();
+    public Material material_first_pass;
+    public Material material_second_pass;
+    public CommandBuffer command_buffer;
+    public Camera camera;
+    public ComputeShader compute_ldi;
+    public RenderTexture depth_complexities;
+    MeshFilter mesh_filter;
+
+    public uint max_ldi_hierarchy_size = 16;
+    void Start()
+    {
+        if(gameObject.TryGetComponent<MeshFilter>(out mesh_filter))
+        {
+            for (int i = 0; i < max_ldi_hierarchy_size; i++)
+            {
+                ldi_hierarchy.Add(new RenderTexture(ldi_resolution.x, ldi_resolution.y,
+                    UnityEngine.Experimental.Rendering.GraphicsFormat.R8G8B8A8_SNorm,
+                    UnityEngine.Experimental.Rendering.GraphicsFormat.D24_UNorm_S8_UInt,
+                    0));
+                ldi_hierarchy[i].depthStencilFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.D24_UNorm_S8_UInt;
+                ldi_hierarchy[i].stencilFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R8_UInt;
+                ldi_hierarchy[i].enableRandomWrite = true;
+
+                command_buffer = new CommandBuffer();
+
+                depth_complexities = new RenderTexture(ldi_resolution.x, ldi_resolution.y, 0, RenderTextureFormat.R8, 0);
+                depth_complexities.enableRandomWrite = true;
+            }
+           
+        }    
+    }
+
+    void render_ldi_layer(Material material, int ldi_hierarchy_target_index)
+    {
+        command_buffer.Clear();
+        command_buffer.SetRenderTarget(ldi_hierarchy[ldi_hierarchy_target_index], 0);
+        command_buffer.SetViewProjectionMatrices(camera.worldToCameraMatrix, camera.projectionMatrix);
+
+        command_buffer.ClearRenderTarget(RTClearFlags.All, Color.clear, 1.0f, 0);
+        command_buffer.DrawMesh(mesh_filter.sharedMesh, transform.localToWorldMatrix, material, 0, 0);
+        Graphics.ExecuteCommandBuffer(command_buffer);
+        command_buffer.Clear();
+    }
+
+    void render_ldi_hierarchy()
+    {
+        material_first_pass.SetInteger("StencilRef", 1);
+        render_ldi_layer(material_first_pass, 0);
+        for (int i = 1; i < max_ldi_hierarchy_size; i++)
+        {
+            material_second_pass.SetInteger("StencilRef", i + 1);
+            render_ldi_layer(material_second_pass, i);
+        }
+    }
+
+    // sort ldi_hierarchy[index] and ldi_hierarchy[index+1]
+    void _sort_ldi_hierarchy_pass(int comparison_index)
+    {
+        int kernel = compute_ldi.FindKernel("sort_textures");
+        compute_ldi.SetTexture(kernel, "texture_a", ldi_hierarchy[comparison_index]);
+        compute_ldi.SetTexture(kernel, "texture_b", ldi_hierarchy[comparison_index + 1]);
+        compute_ldi.SetVector("texture_resolution", new Vector2(ldi_hierarchy[comparison_index].width, ldi_hierarchy[comparison_index].height));
+
+        compute_ldi.Dispatch(kernel, Mathf.CeilToInt(ldi_hierarchy[comparison_index].width / 8.0f), Mathf.CeilToInt(ldi_hierarchy[comparison_index].height / 8.0f), 1);
+    }
+
+    void sort_ldi_hierarchy()
+    {
+        for (int iteration = 0; iteration < ldi_hierarchy.Count; iteration++)
+        {
+            for (int i = 0; i < ldi_hierarchy.Count - 1; i++)
+            {
+                _sort_ldi_hierarchy_pass(i);
+            }
+        }
+    }
+
+    void blit_stencil_to_color_texture(RenderTexture source_stencil, RenderTexture target_color)
+    {
+        int kernel = compute_ldi.FindKernel("blit_stencil_to_color");
+        compute_ldi.SetTexture(kernel, "stencil_texture", source_stencil, 0, RenderTextureSubElement.Stencil);
+        compute_ldi.SetTexture(kernel, "target_color_texture", target_color);
+        compute_ldi.SetVector("texture_resolution", new Vector2(source_stencil.width, source_stencil.height));
+        compute_ldi.SetFloat("max_stencil_value", (float)ldi_hierarchy.Count);
+
+        compute_ldi.Dispatch(kernel, Mathf.CeilToInt(source_stencil.width / 8.0f), Mathf.CeilToInt(source_stencil.height / 8.0f), 1);
+    }
+
+
+    // sort ldi_hierarchy[index] and ldi_hierarchy[index+1]
+    void _sort_ldi_hierarchy_with_depth_complexity_pass(RenderTexture depth_complexities, int hierarchy_size, int comparison_index)
+    {
+        int kernel = compute_ldi.FindKernel("sort_textures_with_depth_complexities");
+        compute_ldi.SetTexture(kernel, "texture_a", ldi_hierarchy[comparison_index]);
+        compute_ldi.SetTexture(kernel, "texture_b", ldi_hierarchy[comparison_index + 1]);
+        compute_ldi.SetVector("texture_resolution", new Vector2(ldi_hierarchy[comparison_index].width, ldi_hierarchy[comparison_index].height));
+
+        compute_ldi.SetTexture(kernel, "depth_complexities", depth_complexities);
+        compute_ldi.SetFloat("depth_complexities_max_value", hierarchy_size);
+        compute_ldi.SetInt("comperison_index", comparison_index);
+
+        compute_ldi.Dispatch(kernel, Mathf.CeilToInt(ldi_hierarchy[comparison_index].width / 8.0f), Mathf.CeilToInt(ldi_hierarchy[comparison_index].height / 8.0f), 1);
+    }
+
+    void sort_ldi_hierarchy_with_depth_complexity(RenderTexture depth_complexities)
+    {
+        for (int iteration = 0; iteration < ldi_hierarchy.Count; iteration++)
+        {
+            for (int i = 0; i < ldi_hierarchy.Count - 1; i++)
+            {
+                _sort_ldi_hierarchy_with_depth_complexity_pass(depth_complexities, ldi_hierarchy.Count, i);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (mesh_filter == null)
+            return;
+
+        render_ldi_hierarchy();
+        blit_stencil_to_color_texture(ldi_hierarchy[0], depth_complexities);
+
+        sort_ldi_hierarchy_with_depth_complexity(depth_complexities);
+    }
+}
